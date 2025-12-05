65fbf2196a7fc45a0308042ee8a1b6c4831af83b
Dmitro44 <sebe-lev1@mail.ru>
2025-12-06 00:03:37 +0300

    Another fix of run scripts for intel

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,7 @@
 
 # Exit immediately if a command exits with a non-zero status.
 set -e
+set -x
 
 # --- Core Configuration ---
 # The first argument is a space-separated string of physical core counts to test, e.g., "2 4 6 8"
@@ -126,22 +127,19 @@ run_benchmark_set() {
                     # Intel hybrid: P-cores at 0,2,4,6,8 (even), E-cores at 9,10,11,...
                     # When SMT off, available CPUs are: 0,2,4,6,8,9,10,11,...
                     local INTEL_CPUS=""
-                    local p_cores_used=0
-                    local e_cores_used=0
-                    local cpus_assigned=0
-                    # First assign P-cores (even: 0,2,4,6,8)
-                    while [ $cpus_assigned -lt $NUM_THREADS ] && [ $p_cores_used -lt 5 ]; do
+                    local p_core_idx=0
+                    local e_core_idx=0
+                    for ((i = 0; i < NUM_THREADS; i++)); do
                         [ -n "$INTEL_CPUS" ] && INTEL_CPUS+=","
-                        INTEL_CPUS+="$((p_cores_used * 2))"
-                        ((p_cores_used++))
-                        ((cpus_assigned++))
-                    done
-                    # Then assign E-cores (9,10,11,...)
-                    while [ $cpus_assigned -lt $NUM_THREADS ]; do
-                        [ -n "$INTEL_CPUS" ] && INTEL_CPUS+=","
-                        INTEL_CPUS+="$((9 + e_cores_used))"
-                        ((e_cores_used++))
-                        ((cpus_assigned++))
+                        # First, use up to 4 P-cores (0, 2, 4, 6)
+                        if [ $p_core_idx -lt 4 ]; then
+                            INTEL_CPUS+="$((p_core_idx * 2))"
+                            ((p_core_idx++))
+                        # Then, use E-cores (9, 10, 11, ...)
+                        else
+                            INTEL_CPUS+="$((9 + e_core_idx))"
+                            ((e_core_idx++))
+                        fi
                     done
                     CPU_LIST="$INTEL_CPUS"
                 else
